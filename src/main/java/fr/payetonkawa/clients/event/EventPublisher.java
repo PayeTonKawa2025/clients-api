ee48d6ba88df9d2a80e218e4f7b527490c6d9bdc
lucasdpt <github@zertus.fr>
2025-08-18 21:42:33 +0200

    feat: update EventPublisher

--- src/main/java/fr/payetonkawa/clients/event/EventPublisher.java
+++ src/main/java/fr/payetonkawa/clients/event/EventPublisher.java
@@ -1,6 +1,8 @@
 package fr.payetonkawa.clients.event;
 
+import com.google.gson.Gson;
 import fr.payetonkawa.clients.config.RabbitMQConfig;
+import fr.payetonkawa.common.exchange.ExchangeQueues;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +11,13 @@ public class EventPublisher {
 
     private final AmqpTemplate amqpTemplate;
 
+    private static final Gson gson = new Gson();
+
     public EventPublisher(AmqpTemplate amqpTemplate) {
         this.amqpTemplate = amqpTemplate;
     }
 
     public void sendEvent(String routingKey, Object payload) {
-        amqpTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, routingKey, payload);
+        amqpTemplate.convertAndSend(ExchangeQueues.EXCHANGE_NAME, routingKey, gson.toJson(payload));
     }
 }
